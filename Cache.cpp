3736bf7f10162f124566d4e6b162cd64d4a47ae1
OrusLiOx <y.lily.n@gmail.com>
2023-04-12 11:15:41 -0500

    modified:   Cache.cpp 	modified:   Main.cpp

--- Cache.cpp
+++ Cache.cpp
@@ -1,5 +1,111 @@
+#include<vector>
+#include<iostream>
+#include<set>
+
+using namespace std;
+
 class Cache
 {
 public:
+    Cache(unsigned e, unsigned a)
+    {
+        setSize=e/a;
+        assoc = a;
+
+        for(unsigned set = 0; set < a; set++)
+        {
+            entries.push_back(vector<Entry>());
+            for(unsigned k = 0; k<setSize; k++)
+                entries[set].push_back(Entry());
+        }
+    }
+    Cache(){}
+
+    void display()
+    {
+        for(unsigned k = 0; k< assoc; k++)
+        {
+            cout << k << endl;
+            for(int j=0; j<setSize;j++)
+            {
+                cout<< j <<" : ";
+                entries[k][j].display();
+            } 
+        }
+    }
+
+    string get(unsigned ref, unsigned addr)
+    {
+        unsigned i = addr%setSize;
+
+        // find if val in cache
+        for(vector<Entry> v: entries)
+        {
+            Entry e = v[i];
+            if(e.isValid())
+            {
+                if(e.getTag() == addr-i)
+                {
+                    e.setRef(ref);
+                    return "HIT";
+                }
+            }
+        }
+
+        // val not in cache
+        // find where to put
+        unsigned oldest = 0;
+        for(unsigned k = 0; k< assoc; k++)
+        {
+            Entry e = entries[k][i];
+            if(!e.isValid())
+            {
+                e.setRef(ref);
+                e.setTag(addr-i);
+                e.setValid(true);
+                return "MISS";
+            }
+            if(e.getRef()<entries[oldest][i].getRef())
+                oldest = k;
+        }
+        Entry e = entries[oldest][i];
+        e.setRef(ref);
+        e.setTag(addr-i);
+        e.setValid(true);
+        return "MISS";
+    }
 private:
+    unsigned assoc, setSize;
+    set<unsigned> accessed;
+    vector<std::vector<Entry>> entries;
+};
+
+class Entry {
+public:
+    Entry()
+    {
+        valid = false;
+        ref = -1;
+    }
+
+    void display()
+    {
+        cout << "Valid: "<< valid;
+        if(valid)
+            cout << " | Tag: " << tag << " | Ref: " << ref;
+    }
+
+    void setTag(int _tag) { tag = _tag; }
+    int getTag() { return tag; }
+
+    void setValid(bool _valid) { valid = _valid; }
+    bool isValid() { return valid; }
+
+    void setRef(int _ref) { ref = _ref; }
+    int getRef() { return ref; }
+
+private:  
+    bool valid;
+    unsigned tag;
+    int ref;
 };
--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,52 @@
 #include<iostream>
 #include<cstdlib>
+#include<fstream>
+#include"Cache.cpp"
 
 using namespace std;
- 
+
 int main(int argc, char*argv[]) 
 {
-  // check if correct number of arguments are passed
-  if (argc < 4) {
-    cout << "Usage: " << endl;
-    cout << "   ./cache num_entries associativity filename" << endl;
-    return 0;
-  }
+// VERIFY ARGUMENTS
+   // check if correct number of arguments are passed
+   if (argc < 4) 
+   {
+      cout << "Usage: " << endl;
+      cout << "   ./cache num_entries associativity filename" << endl;
+      return 0;
+   }
 
-  // move arguments into variables of proper types
-  unsigned entries = atoi(argv[1]);
-  unsigned assoc = atoi(argv[2]);
+   // move arguments into variables of proper types
+   unsigned entries = atoi(argv[1]);
+   unsigned assoc = atoi(argv[2]);
 
-  string filename = argv[3];
+   string filename = argv[3];
 
-  // print arguments
-  cout << "Number of entries: " << entries << endl;
-  cout << "Associativity: " << assoc << endl;
-  cout << "Input File Name: " << filename << endl;
+   // print arguments
+   cout << "Number of entries: " << entries << endl;
+   cout << "Associativity: " << assoc << endl;
+   cout << "Input File Name: " << filename << endl;
+
+// CACHE SIM
+   // loop for every address
+   // send address to cache
+   // cache sends back result
+   // record address + result to file
+   ifstream read;
+   read.open("input");
+   string addr;
+   read >> addr;
+
+   Cache cache;
+   
+   while(addr!="")
+   {
+
+      read >> addr;
+   }
+
+// DONE
+   cout << "Check cach_sim_output for results";
   
-  return 0;
+   return 0;
 }
